fc880446408bfe210d54252cb9cfd810624b170f
it kimopay <it.kimopay@gmail.com>
2021-07-03 11:01:49 +0800

    detail screen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -9,7 +9,17 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DetailMobilePage(place: place);
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints){
+          if(constraints.maxWidth <= 600){
+            return DetailMobilePage(place: place);
+          }else{
+            return DetailWebPage(place: place);
+          }
+        }
+      )
+    );
   }
 }
 
@@ -133,16 +143,151 @@ class DetailMobilePage extends StatelessWidget{
 
 }
 
-class DetailWebPage extends StatelessWidget{
+class DetailWebPage extends StatefulWidget{
   final TourismPlace place;
 
   const DetailWebPage({required this.place});
 
   @override
+  _DetailWebPageState createState() => _DetailWebPageState();
+  @override
+  void dispose() {
+    _scrollController.dispose();
+  }
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 64
+        ),
+        child: Center(
+          child: Container(
+            width: 1200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Wisata',
+                  style: TextStyle(
+                      fontSize: 32
+                  ),
+                ),
+                SizedBox(height: 32,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            child: Image.asset(widget.place.imageAsset),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          SizedBox(height: 16,),
+                          Container(
+                            height: 150,
+                            padding: const EdgeInsets.only(bottom: 16),
+                            child: Scrollbar(
+                              isAlwaysShown: true,
+                              controller: _scrollController,
+                              child: ListView(
+                                controller: _scrollController,
+                                padding: const EdgeInsets.only(bottom: 16),
+                                scrollDirection: Axis.horizontal,
+                                children: widget.place.imageUrls.map((url){
+                                  return Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(10),
+                                          child: Image.network(url)
+                                      )
+                                  );
+                                }).toList(),
+                              ),
+                            )
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 32,),
+                    Expanded(
+                      child: Card(
+                        child: Container(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  widget.place.name,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 30.0,
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: <Widget>[
+                                      Icon(Icons.calendar_today),
+                                      SizedBox(width: 8.0,),
+                                      Text(
+                                        widget.place.openDays,
+                                      )
+                                    ],
+                                  ),
+                                  FavoriteButton()
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Icon(Icons.access_time),
+                                  SizedBox(width: 8.0,),
+                                  Text(
+                                    widget.place.openTime,
+                                  )
+                                ],
+                              ),
+                              SizedBox(height: 8.0),
+                              Row(
+                                children: <Widget>[
+                                  Icon(Icons.monetization_on),
+                                  SizedBox(width: 8.0,),
+                                  Text(
+                                    widget.place.ticketPrice,
+                                  )
+                                ],
+                              ),
+                              Container(
+                                padding: EdgeInsets.symmetric(vertical: 16.0),
+                                child: Text(
+                                  widget.place.description,
+                                  textAlign: TextAlign.justify,
+                                  style: TextStyle(
+                                      fontSize: 16.0
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )
+          ),
+        ),
+      )
     );
   }
-
 }
+final _scrollController = ScrollController();
